45fe82d9e4dcc240e4fbe88da1d6e92fb09f1807
crazycs520 <crazycs520@gmail.com>
2022-01-07 19:58:42 +0800

    init
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- ddl/ddl_worker.go
+++ ddl/ddl_worker.go
@@ -408,7 +408,8 @@ func (w *worker) finishDDLJob(t *meta.Meta, job *model.Job) (err error) {
 			// After rolling back an AddIndex operation, we need to use delete-range to delete the half-done index data.
 			err = w.deleteRange(w.ddlJobCtx, job)
 		case model.ActionDropSchema, model.ActionDropTable, model.ActionTruncateTable, model.ActionDropIndex, model.ActionDropPrimaryKey,
-			model.ActionDropTablePartition, model.ActionTruncateTablePartition, model.ActionDropColumn, model.ActionDropColumns, model.ActionModifyColumn, model.ActionDropIndexes:
+			model.ActionDropTablePartition, model.ActionTruncateTablePartition, model.ActionDropColumn, model.ActionDropColumns, model.ActionModifyColumn, model.ActionDropIndexes,
+			model.ActionAlterTablePartitionMeta:
 			err = w.deleteRange(w.ddlJobCtx, job)
 		}
 	}
