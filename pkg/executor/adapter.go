99434bc12b94a7a0f99a873c6ef739054a7b094a
crazycs520 <crazycs520@gmail.com>
2024-12-05 17:37:49 +0800

    debug
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- pkg/executor/adapter.go
+++ pkg/executor/adapter.go
@@ -2023,7 +2023,7 @@ func (a *ExecStmt) SummaryStmt(succ bool) {
 	stmtExecInfo.Digest = digest.String()
 	stmtExecInfo.PrevSQL = prevSQL
 	stmtExecInfo.PrevSQLDigest = prevSQLDigest
-	stmtExecInfo.PlanGenerator = planGenerator
+	//stmtExecInfo.PlanGenerator = planGenerator
 	//stmtExecInfo.BinaryPlanGenerator = binPlanGen
 	stmtExecInfo.PlanDigest = planDigest
 	//stmtExecInfo.PlanDigestGen = planDigestGen
