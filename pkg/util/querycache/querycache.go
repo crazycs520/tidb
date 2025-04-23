f5312369dd3edf1bc94f63280fe53fe2a178d6c0
crazycs520 <crazycs520@gmail.com>
2025-04-23 21:32:52 +0800

    fix
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- pkg/util/querycache/querycache.go
+++ pkg/util/querycache/querycache.go
@@ -51,12 +51,12 @@ func (c *PreparedStmtCache) Add(k []byte, v *QueryCacheValue) bool {
 	ts := time.Now().Unix()
 	c.Lock()
 	if len(c.FieldTypes) == 0 {
-		c.ResultFields = make([]*resolve.ResultField, 0, len(v.ResultFields))
-		c.ResultFields = append(c.ResultFields, v.ResultFields...)
+		c.ResultFields = make([]*resolve.ResultField, len(v.ResultFields))
+		copy(c.ResultFields, v.ResultFields)
 	}
 	if len(c.cache) < c.capacity {
-		chks := make([]*chunk.Chunk, 0, len(v.Chunks))
-		chks = append(chks, v.Chunks...)
+		chks := make([]*chunk.Chunk, len(v.Chunks))
+		copy(chks, v.Chunks)
 		c.cache[string(k)] = &preparedStmtCacheValue{
 			Chunks: chks,
 			ts:     ts,
