cfc3f2f2a2576dd4ce4f06653916099591d32cbb
crazycs520 <crazycs520@gmail.com>
2025-04-23 21:53:59 +0800

    fix test
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- pkg/util/querycache/querycache_test.go
+++ pkg/util/querycache/querycache_test.go
@@ -27,8 +27,14 @@ import (
 
 func TestQueryCache(t *testing.T) {
 	q1 := &QueryCacheKey{
-		SchemaName: "test1",
-		Sql:        "select * from sbtest1 where a = ?",
+		StmtKey: StmtKey{
+			SchemaName: "test1",
+			Sql:        "select * from sbtest1 where a = ?",
+			Vars: QueryVars{
+				TimeZone: time.Local,
+				SQLMode:  mysql.SetSQLMode(mysql.SQLMode(0), mysql.ModeMsSQL),
+			},
+		},
 		Args: []param.BinaryParam{
 			{
 				Tp:         1,
@@ -37,25 +43,21 @@ func TestQueryCache(t *testing.T) {
 				Val:        []byte{1, 2, 3},
 			},
 		},
-		Vars: QueryVars{
-			TimeZone: time.Local,
-			SQLMode:  mysql.SetSQLMode(mysql.SQLMode(0), mysql.ModeMsSQL),
-		},
 	}
-	assert.Equal(t, q1.hashSize(), len(q1.Hash()))
+	assert.Equal(t, q1.argHashSize(), len(q1.ArgHash()))
 	value, _ := GlobalQueryCache.GetQueryCache(q1)
 	assert.Nil(t, value)
 
 	GlobalQueryCache.AddQueryCache(q1, &QueryCacheValue{
-		ReadTs: 1,
+		Chunks: make([]*chunk.Chunk, 1),
 	})
 
 	value, _ = GlobalQueryCache.GetQueryCache(q1)
 	assert.NotNil(t, value)
-	assert.Equal(t, value.ReadTs, uint64(1))
+	assert.Equal(t, len(value.Chunks), 1)
 
 	q1.Vars.SQLMode = mysql.SetSQLMode(mysql.SQLMode(0), mysql.ModeDb2)
-	q1.hash = nil
+	q1.argHash = nil
 	value, _ = GlobalQueryCache.GetQueryCache(q1)
 	assert.Nil(t, value)
 }
@@ -64,8 +66,14 @@ func genKey(i int) *QueryCacheKey {
 	buf := make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, uint64(i))
 	return &QueryCacheKey{
-		SchemaName: "test1",
-		Sql:        "select * from sbtest1 where a = ?",
+		StmtKey: StmtKey{
+			SchemaName: "test1",
+			Sql:        "select * from sbtest1 where a = ?",
+			Vars: QueryVars{
+				TimeZone: time.Local,
+				SQLMode:  mysql.SetSQLMode(mysql.SQLMode(0), mysql.ModeMsSQL),
+			},
+		},
 		Args: []param.BinaryParam{
 			{
 				Tp:         1,
@@ -74,15 +82,10 @@ func genKey(i int) *QueryCacheKey {
 				Val:        buf,
 			},
 		},
-		Vars: QueryVars{
-			TimeZone: time.Local,
-			SQLMode:  mysql.SetSQLMode(mysql.SQLMode(0), mysql.ModeMsSQL),
-		},
 	}
 }
 
 func genValue() *QueryCacheValue {
-	ts := time.Now().Unix()
 	colLen := 5
 	fieldTypes := make([]*types.FieldType, colLen)
 	resultFields := make([]*resolve.ResultField, colLen)
@@ -111,28 +114,24 @@ func genValue() *QueryCacheValue {
 	chks := make([]*chunk.Chunk, 0)
 	chk := chunk.New(fieldTypes, 1, 1)
 	for i := range fieldTypes {
-		for j := 0; j < 1; j++ {
-			var d types.Datum
-			switch fieldTypes[i].GetType() {
-			case mysql.TypeVarchar:
-				d = types.NewDatum(fmt.Sprintf("abcdefghijklmnopq-" + strconv.Itoa(j)))
-			default:
-				d = types.NewDatum(j)
-			}
-			chk.AppendDatum(i, &d)
+		var d types.Datum
+		switch fieldTypes[i].GetType() {
+		case mysql.TypeVarchar:
+			d = types.NewDatum(fmt.Sprintf("abcdefghijklmnopq-" + strconv.Itoa(i)))
+		default:
+			d = types.NewDatum(i)
 		}
+		chk.AppendDatum(i, &d)
 	}
 	chks = append(chks, chk)
 	return &QueryCacheValue{
-		ReadTs:       uint64(ts),
 		ResultFields: resultFields,
 		Chunks:       chks,
-		ts:           ts,
 	}
 }
 
 func BenchmarkQueryCache(b *testing.B) {
-	cache := NewQueryCache(2000000)
+	cache := NewPreparedQueryCache(1000000)
 	k := genKey(0)
 	v := genValue()
 	for i := 0; i < b.N; i++ {
@@ -149,7 +148,7 @@ func TestQueryCacheMemUsage(t *testing.T) {
 		log.Println(http.ListenAndServe("127.0.0.1:8081", nil))
 	}()
 
-	cache := NewQueryCache(2000000)
+	cache := NewPreparedQueryCache(2000000)
 	count := 1000000
 	memSize := int64(0)
 	for i := 0; i < count; i++ {
@@ -160,6 +159,14 @@ func TestQueryCacheMemUsage(t *testing.T) {
 		cache.AddQueryCache(k, v)
 	}
 	require.Equal(t, count, cache.Len())
+	require.Equal(t, 1, cache.StmtCount())
+
+	k := genKey(0)
+	k.Sql = "select * from sbtest1 where b = ?"
+	v := genValue()
+	cache.AddQueryCache(k, v)
+	require.Equal(t, count+1, cache.Len())
+	require.Equal(t, 2, cache.StmtCount())
 
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
@@ -169,8 +176,6 @@ func TestQueryCacheMemUsage(t *testing.T) {
 	fmt.Printf("\tHeapInUse = %v MiB", m.HeapInuse/1024/1024)
 	fmt.Printf("\tSys = %v MiB", m.Sys/1024/1024)
 	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-
-	time.Sleep(time.Second * 300)
 }
 
 func TestQueryCache2(t *testing.T) {
@@ -181,7 +186,9 @@ func TestQueryCache2(t *testing.T) {
 			defer wg.Done()
 			for j := 0; j < 1000000; j++ {
 				k := &QueryCacheKey{
-					Sql: fmt.Sprintf("sbtest_id_%v", rand.Intn(100000)),
+					StmtKey: StmtKey{
+						Sql: fmt.Sprintf("sbtest_id_%v", rand.Intn(100000)),
+					},
 				}
 
 				v, _ := GlobalQueryCache.GetQueryCache(k)
@@ -190,13 +197,14 @@ func TestQueryCache2(t *testing.T) {
 				}
 
 				v = &QueryCacheValue{
-					ReadTs: 2,
+					Chunks: make([]*chunk.Chunk, 1),
 				}
 				GlobalQueryCache.AddQueryCache(k, v)
-				//require.Less(t, GlobalQueryCache.Len(), 10000+200)
 			}
 		}(i)
 	}
 
 	wg.Wait()
+	require.Equal(t, 100000, GlobalQueryCache.Len())
+	require.Equal(t, 100000, GlobalQueryCache.StmtCount())
 }
